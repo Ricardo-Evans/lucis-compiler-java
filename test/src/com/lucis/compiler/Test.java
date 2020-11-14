160622277fe829035505e2a78820f934cb5f5653
Ricardo Evans <ricardo.a.evans@gmail.com>
2020-11-14 20:06:56 +0800

    Change: Move test to test package

---
+++ test/src/com/lucis/compiler/Test.java
@@ -0,0 +1,14 @@
+package com.lucis.compiler;
+
+import lucis.compiler.Compiler;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        Compiler compiler = new Compiler();
+        compiler.compile(new File("test-source.lux"));
+    }
+}
