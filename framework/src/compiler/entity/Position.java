7bb443c9fd8ee08516d456f7e322f2dd573a08a1
Ricardo Evans <ricardo.a.evans@gmail.com>
2021-07-29 15:24:59 +0800

    Refactor: position is record now

--- framework/src/compiler/entity/Position.java
+++ framework/src/compiler/entity/Position.java
@@ -9,43 +9,10 @@ import java.io.Serializable;
  * @author Ricardo Evans
  * @version 1.0
  */
-public final class Position implements Serializable {
+public record Position(long line, long offset) implements Serializable {
     @Serial
     private static final long serialVersionUID = 8729840806537371432L;
-
     public static final Position ROOT = new Position(1, 1);
-
-    private final long line;
-    private final long offset;
-
-    /**
-     * Construct a position of the given line and offset
-     *
-     * @param line   the given line
-     * @param offset the given offset
-     */
-    public Position(long line, long offset) {
-        this.line = line;
-        this.offset = offset;
-    }
-
-    /**
-     * Get the line
-     *
-     * @return the line
-     */
-    public long line() {
-        return line;
-    }
-
-    /**
-     * Get the offset
-     *
-     * @return the offset
-     */
-    public long offset() {
-        return offset;
-    }
 
     @Override
     public String toString() {
